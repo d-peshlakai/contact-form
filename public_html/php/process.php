510f60d66fcfcf84af43787592b49fd23ef26f34
Diane Peshlakai <dpeshlakai90@gmail.com>
2017-07-12 22:36:44 -0600

    inputed php and javascript code to get mail function going

--- public_html/php/process.php
+++ public_html/php/process.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: STEMulus
- * Date: 7/12/2017
- * Time: 9:40 PM
- */
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+$EmailTo = "dpeshlakai90@gmail.com";
+$Subject = "New Message Received";
+
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success){
+	echo "success";
+}else{
+	echo "invalid";
+}
+
+
